22be5d728df7db5cded909edac9ab122d4abad39
Rakesh Pranav <156773976+RakeshPranav@users.noreply.github.com>
2026-04-01 21:13:28 +0530

    Time: 1033 ms (17.37%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,11 @@
+select d.name as Department, 
+e.name as Employee,
+e.salary as Salary
+from Employee e
+left join Department d
+on e.departmentId = d.id
+where e.salary=(
+    select max(salary)
+    from Employee
+    where departmentId=E.departmentId
+)
