52678824629ee95f1e69e3904b0e012bf2d7c20e
Rakesh Pranav <156773976+RakeshPranav@users.noreply.github.com>
2026-04-09 19:21:32 +0530

    Time: 4765 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,7 @@
+select r.contest_id , 
+       round(
+        (count(r.contest_id)/(select count(*) from Users))*100 , 2
+       ) as percentage
+from Register r
+group by r.contest_id
+order by percentage desc,r.contest_id
