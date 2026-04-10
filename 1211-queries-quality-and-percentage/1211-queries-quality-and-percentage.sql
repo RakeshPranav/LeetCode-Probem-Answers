b174ee5db43755d5a4784d6cdcb1b0257eac9c22
Rakesh Pranav <156773976+RakeshPranav@users.noreply.github.com>
2026-04-10 10:08:24 +0530

    Time: 414 ms (13.09%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -4,7 +4,7 @@ select query_name ,
         ,2
        ) as quality,
        round(
-        sum(rating<3)*100/count(*)
+        sum(case when rating<3 then 1 else 0 end)*100/count(*)
         ,2
        ) as poor_query_percentage
 from Queries
